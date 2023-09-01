12c7141698f65d407d1d84cac7fa73287b608f2d
luc <luc.leonardi@opsonet.com>
2023-09-01 11:29:38 +0200

    css module with sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
-import "../styles/App.css";
+import styles from "./App.module.scss";
 
-function Title() {
+function Title({truc}) {
+  const localStyles = {
+    
+    h1: {
+      color: 'fuchsia',
+      textAlign: truc ? 'center' : 'left',
+    }
+  }
   return (
-    <div className="border-title">
-      <h1>Kasa</h1>
+    <div className="border-title App">
+      {/** styles gérés via un objet js*/}
+      <h1 style={localStyles.h1}>Kasa</h1>
+      {/** styles gérés en module css - bien regarder le nom de classe généré dans le debugger !*/}
+      <p className={styles.p}>test</p>
     </div>
   );
 }
