1661bb59719b431a9bcf38c072b62bb6b63ede72
Belarif <b.ocine@live.fr>
2023-09-04 17:53:02 +0200

    kasa1: sass install and using it for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import "../styles/header.css";
+import "../sass/main.scss";
 
 function Header() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
+import "./sass/main.scss";
 import Header from "./components/Header";
 import reportWebVitals from "./reportWebVitals";
 
