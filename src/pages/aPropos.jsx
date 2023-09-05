deafbcb16b427c373151b8e57f996835e700cff1
Belarif <b.ocine@live.fr>
2023-09-05 16:32:40 +0200

    kasa1: create 'aPropos' page

---
+++ src/pages/aPropos.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../sass/main.scss";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Apropos = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Apropos;
