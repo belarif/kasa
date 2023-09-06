a393dd7db24f7611535b2e1af587dea79242fc27
Belarif <b.ocine@live.fr>
2023-09-06 18:21:34 +0200

    creating Card component and using it on home page

---
+++ src/components/Card.jsx
@@ -0,0 +1,86 @@
+import { Link } from "react-router-dom";
+
+const Card = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+
+      <li>
+        <Link to="/">
+          <figure>
+            <img src="" alt="image1" />
+            <figcaption>
+              <h2>Titre de la location</h2>
+            </figcaption>
+          </figure>
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Card;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import homeBanner from "../assets/home_banner.png";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
 
 const Home = () => {
   return (
@@ -14,6 +15,9 @@ const Home = () => {
         alt="bannière accueil"
         title="Chez vous, partout et ailleurs"
       ></Banner>
+      <main className="main-home">
+        <Card />
+      </main>
       <Footer />
     </React.Fragment>
   );
