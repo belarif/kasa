9d9cdabe3be492fc763fb415c882f57dd07bcb63
Belarif <b.ocine@live.fr>
2023-09-06 12:24:58 +0200

    kasa1: creating banner component and using it for home page

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import homeBanner from "../assets/home_banner.png";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src={homeBanner} alt="image bannière" />
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "../sass/main.scss";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
+      <main className="main-home">
+        <Banner />
+      </main>
       <Footer />
     </React.Fragment>
   );
