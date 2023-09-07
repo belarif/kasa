928dab44f03f17956a1cc737016935b502e167f9
Belarif <b.ocine@live.fr>
2023-09-07 17:11:59 +0200

    kasa1: updating router to react router 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
 import Apropos from "./pages/aPropos";
 import Error from "./pages/error";
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/aPropos",
+    element: <Apropos />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aPropos" element={<Apropos />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
